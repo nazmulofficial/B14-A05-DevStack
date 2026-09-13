5ac29652172967f7e15e4983b065f055e776e0fb
nazmulofficial <nazmulofficial.com@gmail.com>
2026-09-13 22:47:29 +0600

    fix: correct data json path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-
 import { Suspense } from "react";
+
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Tech from "./components/Tech";
@@ -7,25 +7,28 @@ import Techdata from "./components/technogly/Techdata";
 import type { TechType } from "./type/TechType";
 import Footer from "./footer";
 
-const techdata=async():Promise<TechType[]>=>{
-  const res=await fetch('../public/data.json');
-  const data=await res.json();
+const techdata = async (): Promise<TechType[]> => {
+  const res = await fetch("/data.json");
+  const data = await res.json();
   return data;
-}
+};
 
 function App() {
-  const techPromise=techdata();
+  const techPromise = techdata();
+
   return (
     <>
-    <div className="max-w-7xl mx-auto">
-      <Navbar />
-      <Hero />
-      <Tech />
-      <Suspense fallback={<div>Loading...</div>}>
-      <Techdata  techPromise={techPromise} />
-      </Suspense>
-      <Footer />
-    </div>
+      <div className="mx-auto max-w-7xl">
+        <Navbar />
+        <Hero />
+        <Tech />
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Techdata techPromise={techPromise} />
+        </Suspense>
+
+        <Footer />
+      </div>
     </>
   );
 }
