54b3bb9fc58228ff32b386faed0e61fa3a7d25f9
nazmulofficial <nazmulofficial.com@gmail.com>
2026-09-13 19:33:37 +0600

    Add hero banner section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
     <div className="max-w-7xl mx-auto">
       <Navbar />
+      <Hero />
     </div>
     </>
   );
---
+++ src/components/Hero.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import bannerImage from "../assets/banner-stack.png";
+
+const Hero = () => {
+  return (
+    <div className="py-20 flex justify-between items-center">
+      <div>
+        <h2 className="text-6xl font-bold">
+          Build Your Ideal <br /> <span className="bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">Development Stack</span>{" "}
+        </h2>
+        <p className="w-[576px] py-8">
+          Explore frontend, backend, database, and tooling options, compare them
+          side by side, and put together the stack that fits your next project.
+        </p>
+        <div className="flex gap-8 ">
+          <button>
+            {" "}
+            <a
+              href=""
+              className="bg-linear-to-r from-[#F97316] to-[#EC4899] p-2 rounded-lg text-white"
+            >
+              Explore Technologies
+            </a>{" "}
+          </button>
+          <button className="border-3 border-[#1818eb] p-1 rounded-lg w-[170px]">
+            {" "}
+            <a href="">Learn More</a>{" "}
+          </button>
+        </div>
+      </div>
+      <img src={bannerImage} alt="" />
+    </div>
+  );
+};
+
+export default Hero;
