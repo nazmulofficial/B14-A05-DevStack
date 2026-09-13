b1a3d9cd19d4d8c0d5a63df4523867acf5ece2c6
nazmulofficial <nazmulofficial.com@gmail.com>
2026-09-13 14:12:59 +0600

    navbar new update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,37 @@ const Navbar = () => {
       <nav className="flex justify-between items-center py-4">
         <img className="h-[32px]" src={logo} alt="Logo" />
         <ul className="flex justify-between gap-7 font-bold">
-          <li className="text-red-500">Home</li>
-          <li>Technology</li>
-          <li>Projects</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li className="text-red-500">
+            <a href="#home" className="cursor-pointer hover:text-primary">
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#technologies"
+              className="cursor-pointer hover:text-primary"
+            >
+              Technology
+            </a>
+          </li>
+
+          <li>
+            <a href="#projects" className="cursor-pointer hover:text-primary">
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="#about" className="cursor-pointer hover:text-primary">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#contact" className="cursor-pointer hover:text-primary">
+              Contact
+            </a>
+          </li>
         </ul>
         <div className="flex gap-7">
           <button className="btn btn-soft btn-primary">Sign In</button>
